1728bb7bf9eac0e74b8b6fa96c83bf2d3d5db8c9
Danilo Castro <daniloster@gmail.com>
2015-04-21 16:38:43 +0100

    Added more components to the web project.

---
+++ PilotMvc/Content/Scripts/components/common/services/jsonService.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    var Svc = null;
+    define(['app'], function (app) {
+        if (Svc == null) {
+            Svc = ['$http', 'ConfigApp', function ($http, ConfigApp) {
+                return {
+                    postData: function (url, data, success, error) {
+                        $http.post(ConfigApp.getPath(url), data).success(success).error(error);
+                    },
+                    post: function (url, success, error) {
+                        $http.post(ConfigApp.getPath(url)).success(success).error(error);
+                    },
+                };
+            }];
+            app.lazy.factory('JsonService', Svc);
+        }
+    });
+})();
--- PilotMvc/Content/Scripts/components/common/tooltip/tooltip.js
+++ PilotMvc/Content/Scripts/components/common/tooltip/tooltip.js
@@ -1,107 +1,37 @@
 ﻿(function () {
     var loaded = false;
-    define(['app', 'angular'], function (app, angular) {
+    define(['app'], function (app) {
         if (!loaded) {
-
-            app.lazy.factory("PositionService", function () {
+            app.lazy.directive("tooltip", ['ConfigApp', function (ConfigApp) {
+                angular.element('body').after(angular.element('<link href="' + ConfigApp.getPath('/Content/Scripts/components/common/tooltip/style.css') + '" type="text/css" rel="stylesheet" />'));
                 return {
-                    global: function (element) {
-                        var pos = element.offset();
-                        return {
-                            top: pos.top,
-                            left: pos.left
-                        }
-                    }
-                }
-            });
-
-            app.lazy.factory("AlignService", ['PositionService', function (positionSvc) {
-                return {
-                    pos: function (element, tooltip, align) {
-                        //get dimensions including padding+border
-                        var elHeight = element.outerHeight();
-                        var elWidth = element.outerWidth();
-                        var ttWidth = tooltip.outerWidth();
-                        var ttHeight = tooltip.outerHeight();
-                        var elPos = positionSvc.global(element);
-                        switch (align) {
-                            case 'left':
-                                pos = {
-                                    top: elPos.top + elHeight / 2 - ttHeight / 2,
-                                    left: elPos.left - ttWidth
-                                };
-                                break;
-                            case 'right':
-                                pos = {
-                                    top: elPos.top + elHeight / 2 - ttHeight / 2,
-                                    left: elPos.left + elWidth
-                                };
-                                break;
-                            case 'bottom':
-                                pos = {
-                                    top: elPos.top + elHeight,
-                                    left: elPos.left + elWidth / 2 - ttWidth / 2
-                                };
-                                break;
-                            default: //top
-                                pos = {
-                                    top: elPos.top - ttHeight,
-                                    left: elPos.left + elWidth / 2 - ttWidth / 2
-                                };
-                                break;
-                        }
-                        return pos;
-                    }
-                }
-            }]);
-
-            app.lazy.directive("tooltip", ['$compile', 'AlignService', 'ConfigApp', function ($compile, alignSvc, configApp) {
-                angular.element('body').after(angular.element("<link rel='stylesheet' type='text/css' href='" + configApp.getPath("/Content/Scripts/components/common/tooltip/style.css") + "'/>"));
-                var template =
-                 '<div class="tooltip">' +
-                   '<p>{{message}}</p>' +
-                 '</div>';
-                return {
-                    scope: {
-                        message: "@tooltip"
-                    },
-                    link: function (scope, element, attrs) {
-                        var tooltip;
-                        //scope.message = attrs.ngTooltip;
-                        var align = attrs.tooltipAlign;
-                        function remove() {
-                            if (tooltip) {
-                                tooltip.remove();
-                                tooltip = null;
-                            }
+                    restrict: 'A',
+                    link: function (scope, elem, attrs) {
+                        var ttOptions = {
+                            content: attrs.tooltip,
+                            trigger: attrs.tooltipTrigger || 'hover'
                         };
-
-                        function show() {
-                            tooltip = angular.element(attrs.tooltipTemplate || template);
-                            element.after(tooltip);
-
-                            $compile(tooltip)(scope);
-                            scope.$digest();
-
-                            var pos = alignSvc.pos(element, tooltip, align);
-                            tooltip.css({ top: pos.top, left: pos.left });
-
-                            tooltip.hide().fadeIn();
+                        if (!!attrs.tooltipHtml) {
+                            ttOptions.html = true;
                         }
-
-                        element.bind("mouseenter", function () {
-                            remove();
-                            show();
-                        })
-
-                        element.bind("mouseleave", function () {
-                            remove();
-                        })
+                        if (!!attrs.tooltipPlacement) {
+                            ttOptions.placement = attrs.tooltipPlacement;
+                        }
+                        if (!!attrs.tooltipTemplate) {
+                            ttOptions.template = attrs.tooltipTemplate;
+                        }
+                        if (!!attrs.tooltipTitle) {
+                            ttOptions.title = attrs.tooltipTitle;
+                        }
+                        $(elem).popover(ttOptions);
                     }
-                }
+                };
             }]);
-
             loaded = true;
         }
     });
 })();
+/*
+$('.tooltip-info').popover();
+http://getbootstrap.com/javascript/#popovers
+*/
