80f63051ee5d4c3fb9c88d2b7a8ac13b37d91843
Danilo Castro <daniloster@gmail.com>
2015-04-22 17:48:31 -0300

    Fixes for nav-check.

--- PilotMvc/Content/Scripts/components/common/navMenu/navCheck.js
+++ PilotMvc/Content/Scripts/components/common/navMenu/navCheck.js
@@ -1,29 +1,30 @@
 ﻿(function () {
     var loaded = false, active = null, check = function (items, itemPath) {
-        items.each(function (item, index) {
+        items.each(function (index, item) {
             checkItem(item, itemPath);
         });
     }, checkItem = function (item, itemPath) {
-        if (item.attr('href') == itemPath) {
-            item.addClass('nav-item-selected');
+        if (item.attr('href').replace('/#/', '/') == itemPath.replace('/#/', '/')) {
+            item.parent().addClass('nav-item-selected');
         } else {
-            item.removeClass('nav-item-selected');
+            item.parent().removeClass('nav-item-selected');
         }
     };
     define(['angular', 'app'], function (angular, app) {
         if (!loaded) {
-            app.lazy.directive("navCheck", ['ConfigApp', function (configApp) {
+            app.lazy.directive("navCheck", ['ConfigApp', '$location', function (configApp, $location) {
                 angular.element('body').after(angular.element('<link href="' + configApp.getPath('/Content/Scripts/components/common/navMenu/style.css') + '" type="text/css" rel="stylesheet" />'));
                 return {
                     restrict: 'A',
                     link: function ($scope, elem, attrs) {
 
+                        active = elem.attr('href');
+
                         elem.on('click', function () {
-                            active = elem.attr('href');
                             check($('[nav-check]'), active);
                         });
 
-                        checkItem(item, itemPath);
+                        checkItem(elem, $location.path());
                     }
                 };
             }]);
